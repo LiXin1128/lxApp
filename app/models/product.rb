c4eef51a74e625415094e68a4a028c86780d105e
Lixin1128 <jameslixin11@gmail.com>
2016-12-11 23:30:10 -0800

    Admin Role & Moderating User Comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,15 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
-def average_rating
-  comments.average(:rating).to_f
-end
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :colour, presence: true
+  validates :price, presence: true
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
 
   def highest_rating_comment
 	comments.rating_desc.first
@@ -13,5 +19,7 @@ end
   def lowest_rating_comment
 	comments.rating_desc.last
   end
+
+  validates :name, presence: true
 
 end
